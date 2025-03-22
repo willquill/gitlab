abb518a3a8749fdae961b271779e3b52e777425e
Will Coquillette <4646219+willquill@users.noreply.github.com>
2025-03-22 15:44:40 +0000

    successfully implement OIDC with authentik
    Signed-off-by: Will Coquillette <4646219+willquill@users.noreply.github.com>

--- gitlab.rb
+++ gitlab.rb
@@ -28,3 +28,78 @@ gitlab_rails['gitlab_email_subject_suffix'] = " [GitLab]"
 gitlab_rails['gitlab_root_email'] = ENV['GITLAB_ROOT_EMAIL']
 package['modify_kernel_parameters'] = false
 
+# OpenID Connect auth to authenticate Authentik users
+gitlab_rails['omniauth_allow_single_sign_on'] = ['openid_connect']
+gitlab_rails['omniauth_sync_email_from_provider'] = 'openid_connect'
+gitlab_rails['omniauth_sync_profile_from_provider'] = ['openid_connect']
+gitlab_rails['omniauth_sync_profile_attributes'] = ['email']
+gitlab_rails['omniauth_auto_sign_in_with_provider'] = 'openid_connect'
+gitlab_rails['omniauth_auto_link_saml_user'] = true
+gitlab_rails['omniauth_auto_link_user'] = ["openid_connect"]
+gitlab_rails['omniauth_providers'] = [
+  {
+    name: 'openid_connect',
+    label: ENV['OIDC_LABEL'],
+    args: {
+      name: 'openid_connect',
+      scope: ['openid','profile','email'],
+      response_type: 'code',
+      issuer: ENV['OIDC_ISSUER'],
+      discovery: true,
+      client_auth_method: 'query',
+      uid_field: 'preferred_username',
+      send_scope_to_token_endpoint: 'true',
+      pkce: true,
+      client_options: {
+        identifier: ENV['OIDC_CLIENT_ID'],
+        secret: ENV['OIDC_CLIENT_SECRET'],
+        redirect_uri: ENV['OIDC_REDIRECT_URI'],
+        # Authentik users in the "GitLab Admins" group
+        # will be granted admin access in GitLab
+        # (should work but didn't actually work for me)
+        gitlab: {
+          groups_attribute: "groups",
+          admin_groups: ["GitLab Admins"]
+        }
+      },
+      # For users signing in with this provider you configure,
+      # the GitLab username will be set to the "sub" received from the provider
+      # For my provider, this is the "email" claim
+      gitlab_username_claim: 'preferred_username'
+    }
+  }
+]
+# Disables sign-in with username and password
+# gitlab_rails['gitlab_signin_enabled'] = false DOESN'T WORK?
+
+# CAUTION!
+# This allows users to sign in without having a user account first. Define the allowed providers
+# using an array, for example, ["saml", "google_oauth2"], or as true/false to allow all providers or none.
+# User accounts will be created automatically when authentication was successful.
+gitlab_rails['omniauth_allow_single_sign_on'] = ["openid_connect"]
+gitlab_rails['omniauth_block_auto_created_users'] = false
+# This links the OIDC user to the GitLab user
+# gitlab_rails['omniauth_auto_link_user'] = ['openid_connect']
+
+# LDAP to populate users
+# gitlab_rails['ldap_enabled'] = true
+# gitlab_rails['ldap_servers'] = {
+#   'main' => {
+#     'label' => 'LDAP',
+#     'host' =>  'ldap.mydomain.com',
+#     'port' => 636,
+#     'uid' => 'sAMAccountName',
+#     'bind_dn' => 'CN=Gitlab,OU=Users,DC=domain,DC=com',
+#     'password' => '<bind_user_password>',
+#     'encryption' => 'simple_tls',
+#     'verify_certificates' => true,
+#     'timeout' => 10,
+#     'active_directory' => false,
+#     'user_filter' => '(employeeType=developer)',
+#     'base' => 'dc=example,dc=com',
+#     'lowercase_usernames' => 'false',
+#     'retry_empty_result_with_codes' => [80],
+#     'allow_username_or_email_login' => false,
+#     'block_auto_created_users' => false
+#   }
+# }
